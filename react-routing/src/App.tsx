0ce65f6dcea56fa7f9146ab06ae867e9315f1aa8
Capůrka, Josef <josef.capurka@seznam.cz>
2024-09-21 13:04:39 +0200

    react init app with Material UI

---
+++ react-routing/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+// import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div>
+      <Button variant="contained" color="primary">
+        Click
+      </Button>
+    </div>
+  );
+}
+
+export default App;
